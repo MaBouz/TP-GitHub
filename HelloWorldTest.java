7af41cd7b5303771a751c4fd67c0aa9710981b36
Malek Bouzaien <malek_ensi@yahoo.fr>
2025-10-25 13:59:51 +0100

    Ajout Test

---
+++ HelloWorldTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+    @Test
+    public void testAddition() {
+        assertEquals(5, 2 + 3);
+    }
+}
